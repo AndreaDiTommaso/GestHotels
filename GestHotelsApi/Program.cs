b74e7a64e5b256a44d0b3066ed53630ec6fe1e2b
Andrea Di tommaso <andreaditommaso92@gmail.com>
2023-10-06 23:31:31 +0200

    Aggiunte Migrastions e progetto webApi

---
+++ GestHotelsApi/Program.cs
@@ -0,0 +1,34 @@
+using GestHotelsDomain;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HotelDbContext>(
+    options => options.UseSqlServer( builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+using (IServiceScope scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    scope.ServiceProvider.GetService<HotelDbContext>().Database.Migrate();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
