f56a42e217b0745391e20c563177177472b75174
Andrea Di tommaso <andreaditommaso92@gmail.com>
2023-10-07 00:51:53 +0200

    Aggiunti gli altri controller

--- GestHotelsDomain/HotelDbContext.cs
+++ GestHotelsDomain/HotelDbContext.cs
@@ -6,16 +6,16 @@ namespace GestHotelsDomain
 {
     public class HotelDbContext : DbContext
     {
-        public DbSet<Hotel> Hotel { get; set; }
-        public DbSet<Price> Price { get; set; }
-        public DbSet<PriceList> PriceList { get; set; }
-        public DbSet<Hotel> Room { get; set; }
-        public DbSet<Hotel> RoomType { get; set; }
-
+     
         public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options)
         {
 
         }
+        public DbSet<Hotel> Hotel { get; set; }
+        public DbSet<Price> Price { get; set; }
+        public DbSet<PriceList> PriceList { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<RoomType> RoomType { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
